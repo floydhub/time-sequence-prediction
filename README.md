738ac18f8fbe7b8ece6f9d0a5a213b9ef6310f7e
ReDeiPirati <alessio.gozzoli@gmail.com>
2017-10-08 18:00:37 +0200

    Improving porting

--- README.md
+++ README.md
@@ -1,13 +1,75 @@
 # Time Sequence Prediction
-This is a toy example for beginners to start with. It is helpful for learning both pytorch and time sequence prediction. Two LSTMCell units are used in this example to learn some sine wave signals starting at different phases. After learning the sine waves, the network tries to predict the signal values in the future. The results is shown in the picture below.
+This is a toy example for beginners to start with, more in detail: it's a porting of [pytorch/examples/mnist](https://github.com/pytorch/examples/tree/master/time_sequence_prediction) making it usables on [FloydHub](https://www.floydhub.com/). It is helpful for learning both pytorch and time sequence prediction. Two [LSTMCell]() units are used in this example to learn some sine wave signals starting at different phases. After learning the sine waves, the network tries to predict the signal values in the future. The results is shown in the picture below.
+
+![image](https://cloud.githubusercontent.com/assets/1419566/24184438/e24f5280-0f08-11e7-8f8b-4d972b527a81.png)
+
+The initial signal and the predicted results are shown in the image. We first give some initial signals (full line). The network will  subsequently give some predicted results (dash line). It can be concluded that the network can generate new sine waves.
+
 
 ## Usage
 
-```
+```bash
+# Generate Sine wave dataset
 python generate_sine_wave.py
+# Train a 2 LSTM cell model
 python train.py
 ```
 
-## Result
-The initial signal and the predicted results are shown in the image. We first give some initial signals (full line). The network will  subsequently give some predicted results (dash line). It can be concluded that the network can generate new sine waves.
-![image](https://cloud.githubusercontent.com/assets/1419566/24184438/e24f5280-0f08-11e7-8f8b-4d972b527a81.png)
+
+The `train.py` script accepts the following arguments:
+
+```bash
+usage: train.py [-h] [--data DATA] [--lr LR] [--outf OUTF] [--epochs N]
+
+Pytorch Time Sequence Prediction
+
+optional arguments:
+  -h, --help   show this help message and exit
+  --data DATA  path to dataset
+  --lr LR      learning rate (default: 0.01)
+  --outf OUTF  folder to output images and model checkpoints
+  --epochs N   number of epochs to train (default: 8)
+
+```
+
+
+## Architecture
+
+## Run on FloydHub
+
+Here's the commands to training, evaluating and serving your time sequence prediction model on FloydHub.
+
+### Project Setup
+
+Before you start, log in on FloydHub with the [floyd login](http://docs.floydhub.com/commands/login/) command, then fork and init the project:
+
+```bash
+$ git clone https://github.com/ReDeiPirati/time-sequence-prediction.git
+$ cd time-sequence-prediction
+$ floyd init time-sequence-prediction
+```
+
+### Training
+
+Before you start, run `python generate_sine_wave.py` and upload the generated dataset(`traindata.pt`) as FloydHub dataset, following the FloydHub docs: [Create and Upload a Dataset](https://docs.floydhub.com/guides/create_and_upload_dataset/).
+
+Now it's time to run our training on FloydHub. In this example we will train the model for 8 epochs with a gpu instance.
+
+```bash
+floyd run --gpu --env pytorch-0.2  --data redeipirati/datasets/sine-waves/1:input "python train.py"
+```
+
+### Evaluating
+
+### Serve model through REST API
+
+### More resources
+
+Some useful resources on LSTM Cell and Networks:
+- [Understanding LSTM Networks](http://colah.github.io/posts/2015-08-Understanding-LSTMs/)
+- [Understanding LSTM and its diagrams](https://medium.com/@shiyan/understanding-lstm-and-its-diagrams-37e2f46f1714)
+- [Recurrent Neural Networks (RNN) and Long Short-Term Memory (LSTM) - Brandon Rohrer](https://youtu.be/WCUNPb-5EYI)
+
+### Contributing
+
+For any questions, bug(even typos) and/or features requests do not hesitate to contact me or open an issue!
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import os
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
@@ -7,12 +8,29 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import argparse
 
+parser = argparse.ArgumentParser(description='Pytorch Time Sequence Prediction')
+parser.add_argument('--data', default="/input/" ,help='path to dataset')
+parser.add_argument('--lr', type=float, default=0.4, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--outf', default='/output',
+                    help='folder to output images and model checkpoints')
+parser.add_argument('--epochs', type=int, default=8, metavar='N',
+                    help='number of epochs to train (default: 8)')
+
+args = parser.parse_args()
+# CUDA?
+CUDA = torch.cuda.is_available()
+
+# Model
 class Sequence(nn.Module):
     def __init__(self):
         super(Sequence, self).__init__()
         self.lstm1 = nn.LSTMCell(1, 51)
         self.lstm2 = nn.LSTMCell(51, 1)
+        if CUDA:
+            self.lstm1, self.lstm2 = self.lstm1.cuda(), self.lstm2.cuda()
 
     def forward(self, input, future = 0):
         outputs = []
@@ -20,6 +38,8 @@ class Sequence(nn.Module):
         c_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
         h_t2 = Variable(torch.zeros(input.size(0), 1).double(), requires_grad=False)
         c_t2 = Variable(torch.zeros(input.size(0), 1).double(), requires_grad=False)
+        if CUDA:
+            h_t, c_t, h_t2, c_t2 = h_t.cuda(), c_t.cuda(), h_t2.cuda(), c_t2.cuda()
 
         for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
             h_t, c_t = self.lstm1(input_t, (h_t, c_t))
@@ -33,31 +53,36 @@ class Sequence(nn.Module):
         return outputs
 
 
-
 if __name__ == '__main__':
     # set ramdom seed to 0
     np.random.seed(0)
     torch.manual_seed(0)
     # load data and make training set
-    data = torch.load('traindata.pt')
+    data = torch.load(os.path.join(args.data, 'traindata.pt'))
     input = Variable(torch.from_numpy(data[3:, :-1]), requires_grad=False)
     target = Variable(torch.from_numpy(data[3:, 1:]), requires_grad=False)
+    if CUDA:
+        input, target = input.cuda(), target.cuda()
     test_input = Variable(torch.from_numpy(data[:3, :-1]), requires_grad=False)
     test_target = Variable(torch.from_numpy(data[:3, 1:]), requires_grad=False)
+    if CUDA:
+        test_input, test_target = test_input.cuda(), test_target.cuda()
     # build the model
     seq = Sequence()
     seq.double()
     criterion = nn.MSELoss()
+    if CUDA:
+        criterion.cuda()
     # use LBFGS as optimizer since we can load the whole data to train
-    optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
-    #begin to train
-    for i in range(15):
+    optimizer = optim.LBFGS(seq.parameters(), lr=args.lr)
+    # begin to train
+    for i in range(args.epochs):
         print('STEP: ', i)
         def closure():
             optimizer.zero_grad()
             out = seq(input)
             loss = criterion(out, target)
-            print('loss:', loss.data.numpy()[0])
+            print('loss:', loss.data.cpu().numpy()[0])
             loss.backward()
             return loss
         optimizer.step(closure)
@@ -65,8 +90,8 @@ if __name__ == '__main__':
         future = 1000
         pred = seq(test_input, future = future)
         loss = criterion(pred[:, :-future], test_target)
-        print('test loss:', loss.data.numpy()[0])
-        y = pred.data.numpy()
+        print('test loss:', loss.data.cpu().numpy()[0])
+        y = pred.data.cpu().numpy()
         # draw the result
         plt.figure(figsize=(30,10))
         plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
@@ -80,5 +105,8 @@ if __name__ == '__main__':
         draw(y[0], 'r')
         draw(y[1], 'g')
         draw(y[2], 'b')
-        plt.savefig('predict%d.pdf'%i)
+        plt.savefig(os.path.join(args.outf, 'predict%d.png'%i))
         plt.close()
+
+    # Do checkpointing - Is saved in outf
+    torch.save(seq.state_dict(), '%s/sine_waves_lstm_model_%d_epochs.pth' % (args.outf, args.epochs))
